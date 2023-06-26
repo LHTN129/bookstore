6a816bf52a179bf05f6327d8cce0807429092dce
LHTN129 <93519311+LHTN129@users.noreply.github.com>
2023-06-26 21:37:44 +0700

    Add files via upload

--- src/pages/bookdetails.js
+++ src/pages/bookdetails.js
@@ -1,47 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import { supabase } from "src/database/supabase";
 import Link from "next/link";
 
-export default function BookDetails() {
-  const router = useRouter();
-  console.log(router);
-  const bookId = router.query.id;
-  const [book, setBook] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchBookDetails = async () => {
-      try {
-        const { data, error } = await supabase
-          .from("books")
-          .select("name, desc, buy, isbn10, price")
-          .eq("id", bookId)
-          .single();
-
-        if (error) {
-          throw error;
-        }
-
-        setBook(data);
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-
-    if (bookId) {
-      fetchBookDetails();
-    }
-  }, [bookId]);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!book) {
-    return <div>Loading...</div>;
-  }
-
+export default function BookDetails({ book }) {
   const { name, desc, buy, isbn10, price } = book;
 
   return (
@@ -49,12 +9,38 @@ export default function BookDetails() {
       <h1>Book Details</h1>
       <Link href="/">homepage</Link>
       <h2>Name: {name}</h2>
-      <p>Description: {book.desc}</p>
+      <p>Description: {desc}</p>
       <p>
-        Buy link: <a href="{book.buy}">{book.buy} </a>
+        Buy link: <a href={buy}>{buy}</a>
       </p>
-      <p>ISBN-10: {book.isbn10}</p>
-      <p>Price: {book.price}</p>
+      <p>ISBN-10: {isbn10}</p>
+      <p>Price: {price}</p>
     </div>
   );
+}
+
+export async function getServerSideProps({ query }) {
+  const bookId = query.id;
+
+  try {
+    const { data, error } = await supabase
+      .from("books")
+      .select("name, desc, buy, isbn10, price")
+      .eq("id", bookId)
+      .single();
+
+    if (error) {
+      throw error;
+    }
+
+    const book = data;
+    return {
+      props: { book },
+    };
+  } catch (error) {
+    console.error("Error fetching book details:", error);
+    return {
+      props: { book: null },
+    };
+  }
 }
